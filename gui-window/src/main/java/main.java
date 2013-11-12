0ae2fb951200a7e2a5d8fe0cff4f654eb1fb9400
Łukasz Szarkowicz <szarkowicz@gmail.com>
2013-11-12 23:13:10 +0100

    Uaktualnienie GUI - dodanie sliderów

--- gui-window/src/main/java/main.java
+++ gui-window/src/main/java/main.java
@@ -25,6 +25,8 @@ public class main extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -72,67 +74,116 @@ public class main extends JFrame {
 		panel.setLayout(null);
 		
 		JLabel parametr1 = new JLabel("Rozmiar populacji:");
-		parametr1.setBounds(31, 34, 134, 16);
+		parametr1.setBounds(28, 81, 134, 16);
 		panel.add(parametr1);
 		
 		JLabel parametr2 = new JLabel("Liczba iteracji:");
-		parametr2.setBounds(31, 62, 134, 16);
+		parametr2.setBounds(28, 109, 134, 16);
 		panel.add(parametr2);
 		
 		JLabel parametr3 = new JLabel("Prawdopodobie\u0144stwo mutacji:");
-		parametr3.setBounds(31, 98, 266, 16);
+		parametr3.setBounds(28, 145, 266, 16);
 		panel.add(parametr3);
 		
 		textField = new JTextField();
-		textField.setBounds(163, 28, 134, 28);
+		textField.setBounds(160, 75, 134, 28);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(163, 56, 134, 28);
+		textField_1.setBounds(160, 103, 134, 28);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JSlider slider = new JSlider();
-		slider.setBounds(31, 118, 190, 29);
+		slider.setBounds(28, 165, 190, 29);
 		panel.add(slider);
 		
 		JLabel lblNewLabel = new JLabel("Prawdopodobie\u0144stwo krzy\u017Cowania:");
-		lblNewLabel.setBounds(31, 158, 266, 16);
+		lblNewLabel.setBounds(28, 205, 266, 16);
 		panel.add(lblNewLabel);
 		
 		JSlider slider_1 = new JSlider();
-		slider_1.setBounds(31, 178, 190, 29);
+		slider_1.setBounds(28, 225, 190, 29);
 		panel.add(slider_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Metody selekcji:");
-		lblNewLabel_1.setBounds(423, 34, 170, 16);
+		lblNewLabel_1.setBounds(321, 34, 170, 16);
 		panel.add(lblNewLabel_1);
 		
 		JRadioButton rdbtnMetoda = new JRadioButton("metoda1");
-		rdbtnMetoda.setBounds(501, 61, 141, 23);
+		rdbtnMetoda.setBounds(399, 61, 141, 23);
 		panel.add(rdbtnMetoda);
 		
 		JRadioButton rdbtnMetoda_1 = new JRadioButton("metoda2");
-		rdbtnMetoda_1.setBounds(501, 91, 141, 23);
+		rdbtnMetoda_1.setBounds(399, 91, 141, 23);
 		panel.add(rdbtnMetoda_1);
 		
 		JRadioButton rdbtnMetoda_2 = new JRadioButton("metoda3");
-		rdbtnMetoda_2.setBounds(501, 121, 141, 23);
+		rdbtnMetoda_2.setBounds(399, 121, 141, 23);
 		panel.add(rdbtnMetoda_2);
 		
 		JRadioButton rdbtnMetoda_3 = new JRadioButton("metoda4");
-		rdbtnMetoda_3.setBounds(501, 151, 141, 23);
+		rdbtnMetoda_3.setBounds(399, 151, 141, 23);
 		panel.add(rdbtnMetoda_3);
 		
 		JLabel lblOkresPredykacji = new JLabel("Okres predykacji:");
-		lblOkresPredykacji.setBounds(423, 209, 170, 16);
+		lblOkresPredykacji.setBounds(321, 209, 170, 16);
 		panel.add(lblOkresPredykacji);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(433, 226, 160, 28);
+		textField_2.setBounds(331, 226, 160, 28);
 		panel.add(textField_2);
 		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Szereg czasowy:");
+		lblNewLabel_2.setBounds(28, 53, 134, 16);
+		panel.add(lblNewLabel_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(160, 47, 134, 28);
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblOknoCzasowe = new JLabel("Okno czasowe:");
+		lblOknoCzasowe.setBounds(28, 25, 134, 16);
+		panel.add(lblOknoCzasowe);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(160, 19, 134, 28);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblProcentOsobnikwPozostwionych = new JLabel("Procent osobnik—w pozostwionych po:");
+		lblProcentOsobnikwPozostwionych.setBounds(528, 34, 298, 16);
+		panel.add(lblProcentOsobnikwPozostwionych);
+		
+		JLabel lblProcentPoSelekcji = new JLabel("Selekcji");
+		lblProcentPoSelekcji.setLabelFor(lblProcentPoSelekcji);
+		lblProcentPoSelekcji.setBounds(528, 71, 315, 16);
+		panel.add(lblProcentPoSelekcji);
+		
+		JLabel lblProcentPoKrzyzowaniu = new JLabel("Krzyýowaniu");
+		lblProcentPoKrzyzowaniu.setLabelFor(lblProcentPoKrzyzowaniu);
+		lblProcentPoKrzyzowaniu.setBounds(528, 101, 315, 16);
+		panel.add(lblProcentPoKrzyzowaniu);
+		
+		JLabel lblProcentPoMutacji= new JLabel("Mutacji");
+		lblProcentPoMutacji.setLabelFor(lblProcentPoMutacji);
+		lblProcentPoMutacji.setBounds(528, 131, 315, 16);
+		panel.add(lblProcentPoMutacji);
+		
+		JSlider sliderSelekcji = new JSlider();
+		sliderSelekcji.setBounds(611, 61, 190, 29);
+		panel.add(sliderSelekcji);
+		
+		JSlider sliderKrzyzowania = new JSlider();
+		sliderKrzyzowania.setBounds(611, 91, 190, 29);
+		panel.add(sliderKrzyzowania);
+		
+		JSlider sliderMutacji = new JSlider();
+		sliderMutacji.setBounds(611, 121, 190, 29);
+		panel.add(sliderMutacji);
 		
 		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.addTab("Chart", null, tabbedPane_2, null);
