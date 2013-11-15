e1935b2c51932ab81df73bbe9c39932a05eea32f
Pawel Synowiec <pawel.synowiec@gmail.com>
2013-11-15 16:53:38 +0100

    staty

--- statistics/src/statistics/Statistics.java
+++ statistics/src/statistics/Statistics.java
@@ -17,54 +17,109 @@ import org.jfree.data.time.TimeSeries;
  */
 public class Statistics {
 
-    public Statistics() {
+    private double[] d1, d2;
+    private double[][] confInt;
+
+    public Statistics(TimeSeries s1, TimeSeries s2) {
+        d1 = timeSeriesToDoubles(s1);
+        d2 = timeSeriesToDoubles(s2);
     }
 
     /**
-     * Wyznacz wspolczynniki wzajemnej korelacji dwo³ch szeregow czasowych tej
-     * samej dlugosci
+     * Wyznacz wspolczynniki wzajemnej korelacji dwoch szeregow czasowych dl n,
+     * n>3
      *
-     * @param s1 Tablica n - pierwszy szereg czasowy
-     * @param s2 Tablica n - drugi szereg czasowy
-     * @return tablica 2*n-1 elementowa wspolczynnikow korelacji
+     * @return tablica 2*n-7 elementowa wspolczynnikow korelacji
      */
-    public double[] getCorrelationCoefficients(TimeSeries s1, TimeSeries s2) {
+    public double[] getCorrelationCoefficients() {
 
-        double[] d1 = timeSeriesToDoubles(s1);
-        double[] d2 = timeSeriesToDoubles(s2);
+        double[] cc = new double[2 * d1.length - 7];
+        confInt = new double[cc.length][2];
+        PearsonsCorrelation pc = new PearsonsCorrelation();
 
-        SpearmansCorrelation pc = new SpearmansCorrelation();
-        int l;
-        double[] cc = new double[2 * d1.length - 1];
-
-        for (int i = 0; i < d1.length; i++) {
-            cc[i + d1.length] = pc.correlation(Arrays.copyOfRange(d1, i, d1.length - 1),
-                    Arrays.copyOfRange(d2, 0, d1.length - i - 1));
+        // korelacja dla opoznien dodatnich
+        for (int i = 0; i < d1.length - 3; i++) {
+            double[] p1 = Arrays.copyOfRange(d1, i, d1.length);
+            double[] p2 = Arrays.copyOfRange(d2, 0, d1.length - i);
+            cc[i + d1.length - 4] = pc.correlation(p1, p2);
+            confInt[i + d1.length - 4] = findConfidenceIntervals(cc[i + d1.length - 4], d1.length);
         }
 
-        for (int i = d1.length; i > 0; i--) {
-            cc[d1.length - i] = pc.correlation(Arrays.copyOfRange(d1, 0, d1.length - i - 1),
-                    Arrays.copyOfRange(d2, i, d1.length - 1));
+        //korelacja dla opoznien ujemnych
+        for (int i = 1; i < d1.length - 3; i++) {
+            double[] p1 = Arrays.copyOfRange(d1, 0, d1.length - i);
+            double[] p2 = Arrays.copyOfRange(d2, i, d1.length);
+            cc[d1.length - i - 4] = pc.correlation(p1, p2);
+            confInt[d1.length - i - 4] = findConfidenceIntervals(cc[d1.length - i - 4], d1.length);
         }
-
         return cc;
     }
 
     /**
-     * Wyznacz srednia arytmetyczna (estymator wartosci oczekiwanej)
+     * Wyznaczenie limitow wartosci wsp korelacji w 95% przedziale ufnosci
      *
-     * @param data Tablica wartosci szeregu czasowego
-     * @return
+     * @param r wspolczynnik korelacji
+     * @param size wielkosc probki
+     * @return dolna i gorna granica przedzialu ufnosci
      */
-    public double getMean(double[] data) {
-        return StatUtils.mean(data);
+    private double[] findConfidenceIntervals(double r, int size) {
+        double n3;
+        n3 = Math.sqrt(1.0 / (size - 3));
+        double z = 0.5 * Math.log((1 + r) / (1 - r));
+
+        double lowR = z - 1.96 * n3;
+        lowR = (Math.exp(2 * lowR) - 1) / (Math.exp(2 * lowR) + 1);
+
+        double upR = z + 1.96 * n3;
+        upR = (Math.exp(2 * upR) - 1) / (Math.exp(2 * upR) + 1);
+
+        return new double[]{lowR, upR};
     }
 
+    /**
+     * zwraca limity wspolczynnikow korelacji dla 95% przedzialu ufnosci
+     * @return talbica zawierajaca dolne i gorne limity dla kazdego wsp korelacji wzajemnej
+     */
+    public double[][] getConfidenceIntervals() {
+        return confInt;
+    }
 
     /**
-     * 
+     *
+     * @return srednia pierwszego szeregu
+     */
+    public double getMeanOfSeries1() {
+        return StatUtils.mean(d1);
+    }
+
+    /**
+     *
+     * @return srednia drugiego szeregu
+     */
+    public double getMeanOfSeries2() {
+        return StatUtils.mean(d2);
+    }
+
+    /**
+     *
+     * @return wariancja pierwszego szeregu
+     */
+    public double getVarOfSeries1() {
+        return StatUtils.variance(d1);
+    }
+
+    /**
+     *
+     * @return wariancja drugiego szeregu
+     */
+    public double getVarOfSeries2() {
+        return StatUtils.variance(d2);
+    }
+
+    /**
+     *
      * @param s szereg czasowy wejsciowy
-     * @return tablica doubli wartosci 
+     * @return tablica doubli wartosci
      */
     private double[] timeSeriesToDoubles(TimeSeries s) {
         int length = s.getItemCount();
