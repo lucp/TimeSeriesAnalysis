58dc319369d76160205858fde5421c90d0ba0e26
Pawel Synowiec <pawel.synowiec@gmail.com>
2013-11-16 18:15:07 +0100

    finalna wersja statystyki + pom.xml

--- statistics/src/statistics/Statistics.java
+++ statistics/src/statistics/Statistics.java
@@ -6,6 +6,7 @@
 package statistics;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.correlation.*;
 import org.jfree.data.time.TimeSeries;
@@ -18,7 +19,8 @@ import org.jfree.data.time.TimeSeries;
 public class Statistics {
 
     private double[] d1, d2;
-    private double[][] confInt;
+    private double[][] confidenceIntervals;
+    private double[] cc;
 
     public Statistics(TimeSeries s1, TimeSeries s2) {
         d1 = timeSeriesToDoubles(s1);
@@ -26,15 +28,15 @@ public class Statistics {
     }
 
     /**
-     * Wyznacz wspolczynniki wzajemnej korelacji dwoch szeregow czasowych dl n,
-     * n>3
+     * Wyznacza nalepszy wspolczynnik korelacji, tj. o najwiekszej wartosci r/rho, 
+     * r - wsp korelacji, rho - szerokosc przedzialu ufnosci
      *
-     * @return tablica 2*n-7 elementowa wspolczynnikow korelacji
+     * @return najlepszy wspolczynnik korelacji i jego przedzial 95% ufnosci
      */
-    public double[] getCorrelationCoefficients() {
+    public double[] findBestCoefficient() {
 
-        double[] cc = new double[2 * d1.length - 7];
-        confInt = new double[cc.length][2];
+        cc = new double[2 * d1.length - 7];
+        confidenceIntervals = new double[cc.length][2];
         PearsonsCorrelation pc = new PearsonsCorrelation();
 
         // korelacja dla opoznien dodatnich
@@ -42,7 +44,7 @@ public class Statistics {
             double[] p1 = Arrays.copyOfRange(d1, i, d1.length);
             double[] p2 = Arrays.copyOfRange(d2, 0, d1.length - i);
             cc[i + d1.length - 4] = pc.correlation(p1, p2);
-            confInt[i + d1.length - 4] = findConfidenceIntervals(cc[i + d1.length - 4], d1.length);
+            confidenceIntervals[i + d1.length - 4] = findConfidenceIntervals(cc[i + d1.length - 4], d1.length);
         }
 
         //korelacja dla opoznien ujemnych
@@ -50,9 +52,27 @@ public class Statistics {
             double[] p1 = Arrays.copyOfRange(d1, 0, d1.length - i);
             double[] p2 = Arrays.copyOfRange(d2, i, d1.length);
             cc[d1.length - i - 4] = pc.correlation(p1, p2);
-            confInt[d1.length - i - 4] = findConfidenceIntervals(cc[d1.length - i - 4], d1.length);
+            confidenceIntervals[d1.length - i - 4] = findConfidenceIntervals(cc[d1.length - i - 4], d1.length);
         }
-        return cc;
+
+        double[][] bestResults = new double[confidenceIntervals.length][2];
+        for (int i = 0; i < confidenceIntervals.length; i++) {
+            bestResults[i][0] = i;
+            bestResults[i][1] = Math.abs(cc[i]) / (confidenceIntervals[i][1] - confidenceIntervals[i][1]);
+        }
+
+        Arrays.sort(bestResults, new Comparator<double[]>() {
+
+            @Override
+            public int compare(double[] t, double[] t1) {
+                return Double.compare(t[1], t1[1]);
+            }
+        });
+                
+
+        int i = (int) bestResults[0][0];
+
+        return new double[]{cc[i], confidenceIntervals[i][0], confidenceIntervals[i][1]};
     }
 
     /**
@@ -78,12 +98,22 @@ public class Statistics {
 
     /**
      * zwraca limity wspolczynnikow korelacji dla 95% przedzialu ufnosci
-     * @return talbica zawierajaca dolne i gorne limity dla kazdego wsp korelacji wzajemnej
+     *
+     * @return talbica zawierajaca dolne i gorne limity dla kazdego wsp
+     * korelacji wzajemnej
      */
     public double[][] getConfidenceIntervals() {
-        return confInt;
+        return confidenceIntervals;
     }
 
+    /**
+     * zwraca wyznaczone wspolczynniki korelacji wzajemnej szeregow
+     * @return tablica wsp. korelacji
+     */
+    public double[] getCorrelationCoefficients() {
+        return cc;
+    }
+    
     /**
      *
      * @return srednia pierwszego szeregu
@@ -125,7 +155,7 @@ public class Statistics {
         int length = s.getItemCount();
         double[] d = new double[length];
         for (int i = 0; i < length; i++) {
-            d[i] = (double)s.getValue(i);
+            d[i] = (double) s.getValue(i);
         }
 
         return d;
